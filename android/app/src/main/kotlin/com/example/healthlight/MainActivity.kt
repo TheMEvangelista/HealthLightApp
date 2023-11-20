6772a1f0982d628b370540f26d230ea004fe9cd0
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-11-20 16:21:24 -0300

    Projeto Healthlight iniciado

---
+++ android/app/src/main/kotlin/com/example/healthlight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthlight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 80, right: 80, top: 10),
+          child: Image.asset("lib/images/logoHL.png"),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Image.asset("lib/images/logoDoctors.png"),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Text(
+            "Bem-vindo ao HealthLight! Aqui voçê vai encontrar o profissional certo para cuidar da sua saúde.",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 25),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: ((context) {
+                return HomePage();
+              }))),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(9, 232, 94, 10),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.all(20),
+                child: const Text(
+                  "Vamos começar... ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+}
