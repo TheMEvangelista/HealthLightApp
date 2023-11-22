795c92d3fd2e1f10c7fbd700fd3a343843334446
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-11-22 20:41:28 -0300

    Efeito de bolha usando Stack

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,32 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// Gesture Detector Button
+// Row(
+//           mainAxisAlignment: MainAxisAlignment.end,
+//           children: [
+//             GestureDetector(
+//               onTap: () => Navigator.pushReplacement(context,
+//                   MaterialPageRoute(builder: ((context) {
+//                 return const HomePage();
+//               }))),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   color: const Color.fromRGBO(9, 232, 94, 10),
+//                   borderRadius: BorderRadius.circular(15),
+//                 ),
+//                 padding: const EdgeInsets.all(20),
+//                 child: const Text(
+//                   "Vamos começar... ",
+//                   style: TextStyle(
+//                     color: Colors.black,
+//                     fontSize: 18,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,55 +6,37 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 80, right: 80, top: 10),
-          child: Image.asset("lib/images/logoHL.png"),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Image.asset("lib/images/logoDoctors.png"),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 20, right: 20),
-          child: Text(
-            "Bem-vindo ao HealthLight! Aqui voçê vai encontrar o profissional certo para cuidar da sua saúde.",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        const SizedBox(height: 25),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
+      body: Center(
+        child: Stack(
           children: [
-            GestureDetector(
-              onTap: () => Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: ((context) {
-                return const HomePage();
-              }))),
+            Container(
+              alignment: const Alignment(-1.1, -1.6),
               child: Container(
-                decoration: BoxDecoration(
-                  color: const Color.fromRGBO(9, 232, 94, 10),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                padding: const EdgeInsets.all(20),
-                child: const Text(
-                  "Vamos começar... ",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+                height: 250,
+                width: 250,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(188, 92, 16, 224),
+                  shape: BoxShape.circle,
                 ),
               ),
             ),
+            Container(
+              alignment: const Alignment(-1.8, -1.1),
+              child: Container(
+                height: 250,
+                width: 250,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(200, 92, 16, 224),
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            Container(
+              
+            ),
           ],
         ),
-      ]),
+      ),
     );
   }
 }
