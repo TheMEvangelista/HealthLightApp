fe2c44787ebb13e853ca0b36012032c23c08ee53
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-11-25 01:21:15 -0300

    Página de introdução concluída

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:healthlight/pages/home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -6,61 +8,95 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            Container(
-              alignment: const Alignment(-1.3, -1.5),
-              child: Container(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Center(
+              child: Image.asset(
+                'lib/images/logoHL.png',
+                height: 125,
+                width: 125,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              "HealthLight",
+              style: GoogleFonts.aoboshiOne(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(200, 81, 203, 32),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: Center(
+              child: Image.asset(
+                'lib/images/Doctors.png',
                 height: 250,
-                width: 250,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(200, 81, 203, 32),
-                  shape: BoxShape.circle,
-                ),
+                width: 370,
               ),
             ),
-            Container(
-              alignment: const Alignment(-2.3, -1.2),
-              child: Container(
-                height: 250,
-                width: 250,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(200, 81, 203, 32),
-                  shape: BoxShape.circle,
-                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Text(
+              "Bem-vindo a HealthLight! Aqui voçê vai encontrar o profissional certo para cuidar da sua saúde.",
+              style: GoogleFonts.aoboshiOne(
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 170),
-                    child: SizedBox(
-                      height: 250,
-                      width: 250,
-                      child: Image.asset("lib/images/logoHL.png"),
-                    ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: ((context) {
+                  return const HomePage();
+                }))),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(200, 81, 203, 32),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                ],
-              ),
-            ),
-            const Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 100),
-                child: Text(
-                  "HealLight",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    "Vamos começar... ",
+                    style: GoogleFonts.aoboshiOne(fontSize: 20),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
 }
+
+// GestureDetector(
+                
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     color: const Color.fromRGBO(9, 232, 94, 10),
+//                     borderRadius: BorderRadius.circular(15),
+//                   ),
+//                   padding: const EdgeInsets.all(20),
+//                   child: const Text(
+//                     "Vamos começar... ",
+//                     style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 18,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//               ),
