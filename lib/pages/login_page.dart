5e50d1d8068d895267eda285bb56808be9cdf2c5
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-12-13 12:35:11 -0300

    1º página de login pronta

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,45 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:healthlight/Extensions/size_screen.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:healthlight/Colors/palete_colors.dart';
+import 'package:healthlight/Components/custom_button.dart';
+import 'package:healthlight/Components/my_text_field.dart';
 // import 'package:healthlight/pages/intro_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: Colors.blue,
-  //     ),
-  //   );
-  // }
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    // final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: Stack(
         children: [
+          Image.asset(
+            "lib/images/bg-Logo.jpg",
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
           Center(
-            child: Container(
-              width: screenWidth,
-              height: 350,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(200, 81, 203, 32),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(50),
-                  topRight: Radius.circular(50),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              child: const Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text("Create your account"),
+                Image.asset(
+                  'lib/images/logoHL.png',
+                  width: 130,
+                  height: 130,
+                  alignment: Alignment.bottomCenter,
+                ),
+                Text(
+                  "HealLight",
+                  style: GoogleFonts.aoboshiOne(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.blue2,
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: context.screenHeight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: context.screenHeight * 0.63,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          color: AppColors.green1,
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(50),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 27, vertical: 20),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Welcome Back!",
+                                        style: GoogleFonts.aoboshiOne(
+                                            fontSize: 25,
+                                            color: AppColors.white),
+                                      ),
+                                      Text(
+                                        "Faça login para começar",
+                                        style: GoogleFonts.aoboshiOne(
+                                            fontSize: 15,
+                                            color: AppColors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                MyTextField(
+                                  textName: 'Your email',
+                                  textController: _emailController,
+                                  keyboardType: TextInputType.emailAddress,
+                                  color: AppColors.white,
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                MyTextField(
+                                  textName: 'Your Password',
+                                  textController: _passwordController,
+                                  keyboardType: TextInputType.number,
+                                  color: AppColors.white,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                              children: [
+                                CustomButton(
+                                  textName: 'Login',
+                                  onTap: () {},
+                                  color: AppColors.white,
+                                  textSize: 18,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -47,11 +148,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
-//Navigator.pushReplacement(
-                  //   context,
-                  //   MaterialPageRoute(builder: ((context) {
-                  //     return const IntroPage();
-                  //   })),
-                  // );
-
