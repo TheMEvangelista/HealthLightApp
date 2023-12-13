34e9fc862082c316ba444895ad4b398d9e871925
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-12-13 12:32:20 -0300

    Campo de Texto melhorado

--- lib/Components/my_text_field.dart
+++ lib/Components/my_text_field.dart
@@ -1,46 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:healthlight/Colors/palete_colors.dart';
 
 class MyTextField extends StatelessWidget {
-  final controller;
-  final String hintText;
-  final bool obscureText;
-
   const MyTextField({
     super.key,
-    required this.controller,
-    required this.hintText,
-    required this.obscureText,
+    required this.textController,
+    required this.textName,
+    this.keyboardType = TextInputType.text,
+    this.isObscureText = false,
+    this.obscureCharacter = '&',
+    this.prefixIcon,
+    this.color,
   });
+  final String textName;
+  final TextEditingController textController;
+  final TextInputType? keyboardType;
+  final bool? isObscureText;
+  final String? obscureCharacter;
+  final Widget? prefixIcon;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return SizedBox(
+      child: TextFormField(
+        controller: textController,
+        obscureText: isObscureText!,
+        keyboardType: keyboardType,
+        obscuringCharacter: obscureCharacter!,
+        style: GoogleFonts.aoboshiOne(fontSize: 18),
         decoration: InputDecoration(
-          hintText: hintText,
-          enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.white,
+          constraints: BoxConstraints(
+            maxHeight: height * 0.1,
+            maxWidth: width * 0.9,
+          ),
+          filled: true,
+          fillColor: color,
+          hintText: textName,
+          hintStyle: GoogleFonts.aoboshiOne(fontSize: 18),
+          prefixIcon: prefixIcon,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: const BorderSide(
+              color: AppColors.transparent,
             ),
           ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: const BorderSide(
+              color: AppColors.transparent,
+            ),
           ),
-          fillColor: const Color.fromRGBO(13, 71, 161, 1),
-          filled: true,
         ),
       ),
     );
   }
 }
-
-// final usernameController = TextEditingController();
-// final passwordController = TextEditingController();
-
-// MyTextField(controller: usernameController,
-// hintText: "User Name", obscureText: false,);
-// MyTextField(controller: passwordController,
-// hintText: "Password", obscureText: true,);
