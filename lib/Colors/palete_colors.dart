942f6e107d1c9096c8f1fd2aa7a51a5c75be367a
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-12-13 12:30:01 -0300

    Pasta contendo a paleta de cores do app

--- lib/Colors/palete_colors.dart
+++ lib/Colors/palete_colors.dart
@@ -1,5 +1,16 @@
-import 'flutter/material.dart';
+import 'package:flutter/material.dart';
 
-const colorsScheme = ColorScheme[
-  backgroundColor = Color()
-]
+class AppColors {
+  static const Color green1 = Color(0xff04e762);
+  static const Color green2 = Color(0xff5aff15);
+  static const Color blue1 = Color(0xff00a1e4);
+  static const Color blue2 = Color(0xff007fff);
+  static const Color red1 = Color(0xfffb4d3d);
+  static const Color red2 = Color(0xffc42021);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color whiteBlue = Color(0xffc2dfe3);
+  static const Color greeBlue = Color(0xff00ffcd);
+  static const Color blueGreen = Color(0xff6fffe9);
+  static const Color transparent = Colors.transparent;
+}
