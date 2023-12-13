e07cc983be301f352c8731364bd8f3eea32110d3
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-12-13 12:31:33 -0300

    Custom Button melhorado

--- lib/Components/custom_button.dart
+++ lib/Components/custom_button.dart
@@ -3,28 +3,77 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CustomButton extends StatelessWidget {
   final String textName;
+  final Color? color;
+  final Color? colorText;
+  final double? textSize;
+  final VoidCallback onTap;
+  final double? bWidth;
+  final double? bHeight;
 
-  const CustomButton({super.key, required this.textName});
+  const CustomButton({
+    super.key,
+    required this.textName,
+    required this.onTap,
+    this.color,
+    this.bWidth,
+    this.bHeight,
+    this.colorText,
+    this.textSize,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-      },
-      style: ElevatedButton.styleFrom(
-        shape: const StadiumBorder(),
-        elevation: 5,
-        minimumSize: const Size.fromHeight(60),
-        backgroundColor: const Color.fromARGB(200, 0, 219, 114),
-      ),
-      child: Text(
-        textName,
-        style: GoogleFonts.aoboshiOne(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(
+            bWidth ?? 100,
+            bHeight ?? MediaQuery.of(context).size.height * 0.085,
+          ),
+          backgroundColor: color,
+          elevation: 2.0,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            textName,
+            style: GoogleFonts.aoboshiOne(
+              color: colorText,
+              fontSize: textSize,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
+
+    // return Material(
+    //   child: InkWell(
+    //     onTap: onTap,
+    //     borderRadius: BorderRadius.circular(20),
+    //     child: Ink(
+    //       padding: const EdgeInsets.symmetric(
+    //         horizontal: 70.0,
+    //         vertical: 18.0,
+    //       ),
+    //       decoration: BoxDecoration(
+    //         borderRadius: BorderRadius.circular(20),
+    //         color: color,
+    //       ),
+    //       child: Text(
+    //         textName,
+    //         style: GoogleFonts.aoboshiOne(
+    //           fontWeight: FontWeight.w700,
+    //           color: AppColors.white,
+    //           fontSize: 16.0,
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
