28126ead1499568125f105dc2f4782bc56d0672c
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2023-12-13 12:28:44 -0300

    Pasta contendo arquivo com MediaQuerys que modificam o tamanho da tela

--- lib/functions/size_screen.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension SizeScreen on BuildContext {
-  double get screenHeight => MediaQuery.of(this).size.height;
-  double get screenWidth => MediaQuery.of(this).size.width;
-}
