c8614bcc3f10584296cc059371306861647da8ed
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2024-02-15 16:55:14 -0300

    mod: SignIn screen and ForgotPassword screen routes added

--- lib/routes/rotas.dart
+++ lib/routes/rotas.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/widgets.dart';
+import 'package:medical_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:medical_app/screens/sign_in/sign_in_screen.dart';
 import 'package:medical_app/screens/splash/splash_screen.dart';
-
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   SignInScreen.routeName: (context) => const SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
 };
