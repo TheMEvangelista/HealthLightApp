d2e87d263cb8601c234afb7f9a9b4e0e0ace20f2
Matheus Evangelista <136542579+TheMEvangelista@users.noreply.github.com>
2024-02-15 16:48:18 -0300

    mod: Primary color, bg color, button\input color selected

--- lib/colors/palete_colors.dart
+++ lib/colors/palete_colors.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color greenButton = Color(0xff04e762);
-const Color green2 = Color(0xffa5e65a);
-const Color greeBlue = Color(0xff00ffcd);
-const Color blueGreen = Color(0xff6fffe9);
-const Color blue1 = Color(0xff00a6c0);
-const Color blueButton = Color(0xff007fff);
-const Color blue3 = Color(0xff03045e);
-const Color blue4 = Color(0xff00b4d8);
-const Color redOrange = Color(0xfffb4d3d);
-const Color redWidget = Color(0xffc42021);
-const Color greyLight = Color(0xffb6c3c7);
-const Color greyDark = Color(0xff68797b);
+const Color primaryColor = Color(0xff04e762);
+const Color bgColor = Color(0xfffdfdff);
+const Color secondaryColor = Color(0xff20a0b9);
+const Color textColor = Color(0xff0a1128);
